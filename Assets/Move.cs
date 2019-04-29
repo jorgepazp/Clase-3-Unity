d37795316f05afe1dccc9d5321ecb63c574f2f9e
unknown <jurgensen>
2019-04-29 13:50:00 -0400

    brayan qlo deja de leer

---
+++ Assets/Move.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public Transform Transf;
+    public float Velocidad;
+	// Use this for initialization
+	void Start () {
+        Transf = GetComponent<Transform>();
+        //Transf.position = new Vector3(0,0,0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("holamundo");
+        }
+
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+      //  Debug.Log("Axis x: " + horizontal  +" Axis Y: "+ vertical);
+       // Debug.Log("Position: " + Transf.position);
+        Transf.position = new Vector2(transform.position.x+horizontal*Velocidad, transform.position.y+vertical*Velocidad);
+    }
+}
